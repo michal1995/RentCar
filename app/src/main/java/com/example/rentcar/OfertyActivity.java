07919c87755851eaceda6b5a1fdba8b5e9898c0a
M Respekta <michal1995.1995@interia.pl>
2019-07-11 12:31:04 +0200

    Initial Commit

--- app/src/main/java/com/example/rentcar/OfertyActivity.java
+++ app/src/main/java/com/example/rentcar/OfertyActivity.java
@@ -3,22 +3,48 @@ package com.example.rentcar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class OfertyActivity extends AppCompatActivity {
+    PomocnikBazaDanych mPomocnikBazaDanych;
 
+     EditText model;
+     EditText typ;
+    EditText rocznik;
+    EditText cena;
+    Button dodajOfertee;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oferty);
-        EditText model = findViewById(R.id.etModel);
-        EditText typ = findViewById(R.id.etTyp);
-        EditText rocznik = findViewById(R.id.etRocznik);
-        EditText cena = findViewById(R.id.etCena);
-        //mPomocnikBazaDanych = new Po
+         model =(EditText) findViewById(R.id.etModel);
+        typ =(EditText) findViewById(R.id.etTyp);
+        rocznik = (EditText)findViewById(R.id.etRocznik);
+        cena =(EditText) findViewById(R.id.etCena);
+       mPomocnikBazaDanych = new PomocnikBazaDanych(this);
+        dodajOfertee = findViewById(R.id.bDodajOferte);
+
+    AddData();
     }
 
+    public void AddData() {
+        dodajOfertee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Boolean newEntry = mPomocnikBazaDanych.addData(model.getText().toString(),typ.getText().toString(),rocznik.getText().toString(),cena.getText().toString());
+                if (newEntry = true) {
+                    Toast.makeText(OfertyActivity.this,"Data Successfully Inserted!",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(OfertyActivity.this,"Wrong :)",Toast.LENGTH_LONG).show();
+                }
 
-
-
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/rentcar/PomocnikBazaDanych.java
+++ app/src/main/java/com/example/rentcar/PomocnikBazaDanych.java
@@ -1,32 +1,60 @@
 package com.example.rentcar;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 
+
 public class PomocnikBazaDanych extends SQLiteOpenHelper {
 
-    private static final String TAG = "pomicnikbazadanych";
-
-    private static final String TABLE_NAME = "car";
+    //private static final String TAG = "pomicnikbazadanych";
+    private static final String DATABASE_NAME = "cars.db";
+    private static final String TABLE_NAME = "carrent";
     private static final String COL1 = "ID";
     private static final String COL2 = "model";
     private static final String COL3 = "typ";
     private static final String COL4 = "rocznik";
     private static final String COL5 = "cena";
 
-    public PomocnikBazaDanych(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public PomocnikBazaDanych(@Nullable Context context) {
+        super(context, TABLE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" + COL1 + " INTEGER PRIMARY KEY AUTOINCREMENT," + COL2 + " TEXT," + COL3 + " TEXT,"+ COL4 +" TEXT,"+ COL5+" TEXT)");
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
+
+    public boolean addData(String model,String typ,String rocznik,String cena) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, model);
+        contentValues.put(COL3, typ);
+        contentValues.put(COL4, rocznik);
+        contentValues.put(COL5, cena);
+
+
+
+        //Log.d(TAG, "addData: Adding " + item + " to " + TABLE_NAME);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        //if date as inserted incorrectly it will return -1
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
